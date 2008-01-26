246b001a16f7edfb4e4cfee4817ab07b85676381
cygnus78 <cygnus78@82c60e0e-060d-0410-9d89-f494f0761558>
2008-01-26 17:18:06 +0000

    Updated year
    git-svn-id: http://brainblast.svn.sourceforge.net/svnroot/brainblast@25 82c60e0e-060d-0410-9d89-f494f0761558

--- src/Puzzle.cpp
+++ src/Puzzle.cpp
@@ -1,6 +1,6 @@
 /* -*- Mode: c++; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4; c-file-style:"stroustrup" -*-
  *
- * @date 2006
+ * @date 2006-2008
  * @author Daniel Bengtsson
  */
 #include "Puzzle.h"
