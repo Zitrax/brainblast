a9f0d387ca8153a272f5aa5cd2c9de9dd5eca73d
Daniel Bengtsson <daniel@bengtssons.info>
2010-01-29 22:00:41 +0100

    Added TextListener

---
+++ src/TextListener.cpp
@@ -0,0 +1,9 @@
+/* -*- Mode: c++; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4; c-file-style:"stroustrup" -*-
+ *
+ * @date 2006-2010
+ * @author Daniel Bengtsson
+ */
+
+#include "TextListener.h"
+
+int TextListener::m_text_id = 0;
